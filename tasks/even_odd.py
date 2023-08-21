79e14dbad1575cc92f60be457f4cd6a6b5f5d9e7
yan.frolov <yan.frolov@osinit.com>
2023-08-21 20:52:27 +0300

    refactor even odd

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -9,10 +9,7 @@ def even_odd(arr: list[int]) -> float:
     Пример:
     even_odd([1, 2, 3, 4, 5]) == 0.8889
     """
-    sum_even = sum([i for i in arr if i % 2 != 0])
-    sum_odd = sum([i for i in arr if i % 2 == 0])
+    sum_even = sum(i for i in arr if i & 1)
+    sum_odd = sum(i for i in arr if not i & 1)
 
-    if sum_odd == 0:
-        return 0
-
-    return sum_even / sum_odd
+    return sum_even / sum_odd if sum_odd else 0

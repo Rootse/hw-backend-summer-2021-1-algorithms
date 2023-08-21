4bdc36c53055535b71f5354cc516d92cad27a3cf
yan.frolov <yan.frolov@osinit.com>
2023-08-21 21:30:48 +0300

    add task prime

--- tasks/prime.py
+++ tasks/prime.py
@@ -7,4 +7,14 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    raise NotImplementedError
+    if number < 2:
+        return False
+
+    if number % 2 == 0:
+        return number == 2
+
+    for divisor in range(3, int(number ** 0.5) + 1, 2):
+        if number % divisor == 0:
+            return False
+
+    return True

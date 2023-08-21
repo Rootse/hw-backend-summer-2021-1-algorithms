d424c4aad9c69dcc93d456879d511d2e2e5f9124
yan.frolov <yan.frolov@osinit.com>
2023-08-21 21:51:54 +0300

    add task seconds to str

--- tasks/timestr.py
+++ tasks/timestr.py
@@ -12,8 +12,15 @@ def seconds_to_str(seconds: int) -> str:
     3700 -> 01h01m40s
     93600 -> 01d02h00m00s
     """
-    raise NotImplementedError
+    days = seconds // 86400
+    hours = (seconds // 3600) % 24
+    minutes = (seconds // 60) % 60
+    remaining_seconds = seconds % 60
 
+    # Format the time representation
+    time_str = '{:02d}d'.format(days) if days else ''
+    time_str += '{:02d}h'.format(hours) if hours or days else ''
+    time_str += '{:02d}m'.format(minutes) if minutes or days or hours else ''
+    time_str += '{:02d}s'.format(remaining_seconds)
 
-
-
+    return time_str

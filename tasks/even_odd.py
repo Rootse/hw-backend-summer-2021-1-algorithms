2702a831fb6eb68021a6833735eead1d2a14fe97
yan.frolov <yan.frolov@osinit.com>
2023-08-21 20:36:24 +0300

    add task even odd

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -9,4 +9,10 @@ def even_odd(arr: list[int]) -> float:
     Пример:
     even_odd([1, 2, 3, 4, 5]) == 0.8889
     """
-    raise NotImplementedError
+    sum_even = sum([i for i in arr if i % 2 != 0])
+    sum_odd = sum([i for i in arr if i % 2 == 0])
+
+    if sum_odd == 0:
+        return 0
+
+    return sum_even / sum_odd
